ab711d9434598bbe18775c9d3a8183a6e43828c2
Midnight Developer <dev@midnight.network>
2026-07-25 14:48:18 +0530

    feat(contract): implement TipJar Compact smart contract with ZK privacy guarantees

---
+++ contract/src/index.ts
@@ -0,0 +1,16 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+
+import { CompiledContract } from "@midnight-ntwrk/midnight-js-protocol/compact-js";
+
+export * from "./managed/tipjar/contract/index.js";
+export * from "./witnesses.js";
+
+import * as CompiledTipJarContract from "./managed/tipjar/contract/index.js";
+import * as Witnesses from "./witnesses.js";
+
+export const CompiledTipJarContractContract = CompiledContract.make<
+  CompiledTipJarContract.Contract<Witnesses.TipJarPrivateState>
+>("TipJar", CompiledTipJarContract.Contract<Witnesses.TipJarPrivateState>).pipe(
+  CompiledContract.withWitnesses(Witnesses.witnesses),
+  CompiledContract.withCompiledFileAssets("./managed/tipjar"),
+);
---
+++ contract/src/witnesses.ts
@@ -0,0 +1,21 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+
+import { Ledger } from "./managed/tipjar/contract/index.js";
+import { WitnessContext } from "@midnight-ntwrk/midnight-js-protocol/compact-runtime";
+
+export type TipJarPrivateState = {
+  readonly secretKey: Uint8Array;
+};
+
+export const createTipJarPrivateState = (secretKey: Uint8Array): TipJarPrivateState => ({
+  secretKey,
+});
+
+export const witnesses = {
+  localSecretKey: ({
+    privateState,
+  }: WitnessContext<Ledger, TipJarPrivateState>): [
+    TipJarPrivateState,
+    Uint8Array,
+  ] => [privateState, privateState.secretKey],
+};
